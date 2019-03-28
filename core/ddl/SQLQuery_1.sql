e13753917bd4efd0d9297fa91e147b290bf44c3a
amit-pandey17 <44652401+amit-pandey17@users.noreply.github.com>
2019-03-28 15:15:44 -0700

    Add files via upload

--- core/ddl/SQLQuery_1.sql
+++ core/ddl/SQLQuery_1.sql
@@ -1,2 +1,5 @@
-SELECT * FROM core.Address;
+SELECT * FROM core.address;
 Select * from core.Address_type;
+SELECT address_key, [TYPE]
+From [Address], Address_type
+Where [Address].address_key=Address_type.[TYPE];
